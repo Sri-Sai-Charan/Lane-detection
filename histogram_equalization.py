8e78c74380867d0c22f449f86b84d79cf2f94792
Sri-Sai-Charan <svellise@umd.edu>
2022-04-05 17:20:33 -0400

    Removing commented code

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -2,12 +2,179 @@
 
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 
-def main():
-    for count in range(25):
-        img = cv.imread('media/problem_1/'+str(count)+'.png')
-        cv.imshow('Frame',img)
-        cv.waitKey(0)
+
+
+# Class containing functions to perform equalization
+class histogram_equalization:
+
+    # Function to generate histogram of input image 
+    def generate_histogram(self, intensity):
+        histogram = np.zeros([256], dtype='uint32')
+        cols = intensity.shape[0]
+        rows = intensity.shape[1]
+        for i in range(cols):
+            for j in range(rows):
+                histogram[intensity[i, j]] += 1
+        return histogram
+
+    # Function to generate cumulative distribution function graph from histogram
+    def generate_cdf(self, intensity):
+        cdf = np.zeros([256])
+        histogram = self.generate_histogram(intensity)
+        temp = histogram/np.sum(histogram)
+        cdf[0] = temp[0]
+        var = temp[0]
+        for i in range(1, len(temp)):
+            var += temp[i]
+            cdf[i] = var
+        return cdf
+
+    # Function to perform histogram equalization of CDF
+    def equalization(self, intensity):
+        equalized_stream = np.copy(intensity)
+        cdf = self.generate_cdf(intensity)
+        cols = intensity.shape[0]
+        rows = intensity.shape[1]
+        for i in range(cols):
+            for j in range(rows):
+                equalized_stream[i, j] = int(cdf[intensity[i, j]]*255)
+        return equalized_stream
+
+
+    # Function to perform adaptive histogram equalization 
+    def adaptive_equalization(self,intensity,tile_size):
         
+        # Blank canvas to create final image
+        blank=np.zeros(intensity.shape,dtype='uint8')
+        
+        i=0
+        j=0
+        
+
+        for j in range (0,intensity.shape[1],tile_size):
+            i=0
+            for i in range(0,intensity.shape[0],tile_size):
+                # Create tiles from image
+                tile= intensity[i:i+tile_size,j:j+tile_size]
+                # Perform equalization on each tile
+                equalized_tile= self.equalization(tile)
+                # Append tile to blank canvas
+                blank[i:i+tile_size,j:j+tile_size]=equalized_tile
+        
+        return blank
+        
+def main():
+    obj = histogram_equalization()
+
+    # Codec to create videowriter object
+    fourcc = cv.VideoWriter_fourcc('M','J','P','G')
+    # Create video for original images
+    # out1 = cv.VideoWriter('original_video.avi', fourcc, 16, (1224, 370))
+    # Create video to record histogram equalized images
+    out2 = cv.VideoWriter('output/Histogram.avi', fourcc, 16, (1224, 370))
+    # Create video to record adaptive histogram equalized images
+    out3 = cv.VideoWriter('output/Adaptive_histogram.avi', fourcc, 16, (1224, 370))
+
+    for count in range(25):
+        image = cv.imread('media/problem_1/'+str(count)+'.png')
+        # image = cv.imread(frame)
+        image_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+        
+        # Split HSV channels
+        h = image_hsv[:, :, 0]
+        s = image_hsv[:, :, 1]
+        v = image_hsv[:, :, 2]
+        
+
+        # out1.write(image)
+        
+        # Ordinary histogram equalization
+        histogram_eq = np.dstack((h, s, obj.equalization(v)))
+        histogram_eq = cv.cvtColor(histogram_eq, cv.COLOR_HSV2BGR)
+        out2.write(histogram_eq)
+        
+        # Adaptive histogram equalization
+        adaptive_eq=np.dstack((h,s,obj.adaptive_equalization(v,50)))
+        adaptive_eq = cv.cvtColor(adaptive_eq, cv.COLOR_HSV2BGR)
+        out3.write(adaptive_eq)
+        
+        # Uncomment to view each frame if required
+        cv.imshow('original',image)
+        cv.imshow('Only hist eq',histogram_eq)
+        cv.imshow('Adaptive eq',adaptive_eq)
+        cv.waitKey(1)
+        # Can be uncommented if histogram plots need to be viewed
+        # fig,(ax1,ax2)=plt.subplots(2)
+        # fig.suptitle('Histogram and CDF for Original Image')
+        # ax1.plot(range(256),obj.generate_histogram(v))
+        # ax1.set_title('Histogram')
+        # ax2.plot(range(256),obj.generate_cdf(v))
+        # ax2.set_title('CDF')
+        # fig.tight_layout()
+        # plt.show()
+
+        # fig,(ax1,ax2)=plt.subplots(2)
+        # fig.suptitle('Histogram and CDF for Histogram Equalized Image')
+        # ax1.plot(range(256),obj.generate_histogram(obj.equalization(v)))
+        # ax1.set_title('Histogram')
+        # ax2.plot(range(256),obj.generate_cdf(obj.equalization(v)))
+        # ax2.set_title('CDF')
+        # fig.tight_layout()
+        # plt.show()
+
+        # fig, (ax1, ax2) = plt.subplots(2)
+        # fig.suptitle('Histogram and CDF for Adaptive Histogram Equalized Image')
+        # ax1.plot(range(256), obj.generate_histogram(obj.adaptive_equalization(v, 50)))
+        # ax1.set_title('Histogram, tile_size = 50')
+        # ax2.plot(range(256), obj.generate_cdf(obj.adaptive_equalization(v, 50)))
+        # ax2.set_title('CDF, tile_size = 50')
+        # fig.tight_layout()
+        # plt.show()
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+# Gather all images under one iteratable variable
+# files = sorted(glob.glob("adaptive_hist_data" + "/*.png"))
+# # print(files)
+
+# # Define object of class histogram equalization
+# obj = histogram_equalization()
+
+
+# for frame in files:
+#     image = cv.imread(frame)
+#     image_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    
+#     # Split HSV channels
+#     h = image_hsv[:, :, 0]
+#     s = image_hsv[:, :, 1]
+#     v = image_hsv[:, :, 2]
+    
+
+#     out1.write(image)
+    
+#     # Ordinary histogram equalization
+#     histogram_eq = np.dstack((h, s, obj.equalization(v)))
+#     histogram_eq = cv.cvtColor(histogram_eq, cv.COLOR_HSV2BGR)
+#     out2.write(histogram_eq)
+    
+#     # Adaptive histogram equalization
+#     adaptive_eq=np.dstack((h,s,obj.adaptive_equalization(v,50)))
+#     adaptive_eq = cv.cvtColor(adaptive_eq, cv.COLOR_HSV2BGR)
+#     out3.write(adaptive_eq)
+    
+#     # Uncomment to view each frame if required
+#     cv.imshow('original',image)
+#     cv.imshow('Only hist eq',histogram_eq)
+#     cv.imshow('Adaptive eq',adaptive_eq)
+#     cv.waitKey(1)
+
+
+
--- straight_lines.py
+++ straight_lines.py
@@ -10,30 +10,23 @@ def find_lanes(frame,frame_post_mask,limits):
     img_canny = cv.Canny(thresh,100,200,2)
     
     contours, hierarchy  = cv.findContours(img_canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    print(limits[1])
     for i in contours:
         if cv.arcLength(i,False)>limits[1]:
             cv.drawContours(frame, [i], -1, (0,255,0), 3)
-            # cv.namedWindow('contours',cv.WINDOW_NORMAL)
-            # cv.imshow('contours',img)
-            print(cv.arcLength(i,False))
 
         if limits[0]> cv.arcLength(i,False) > limits[1]:
             cv.drawContours(frame, [i], -1, (0,0,255), 3)
-        # cv.namedWindow('contours',cv.WINDOW_NORMAL)
-        # cv.imshow('contours',img)
-            print(cv.arcLength(i,False))
+
     return frame
 
 
 def mask_poly(frame,poly_points):
     img=frame.copy()
-    
     img = cv.fillPoly(img,[poly_points],(0,0,0))
-    # cv.namedWindow('post_mask',cv.WINDOW_NORMAL)
-    # cv.imshow('post_mask',img)
-    
+
+                  
     return img            
+
 
 
 def main():
@@ -41,21 +34,23 @@ def main():
     count =0
     poly_points= np.array([[0,0], [0,539],[400,330],[550,330],[959,539],[959,0]])
     limits = [1000,50]
+    fourcc = cv.VideoWriter_fourcc('m','p','4', 'v')
+    out = cv.VideoWriter('output/problem_2_output.avi',cv.VideoWriter_fourcc('M','J','P','G'), 30, (960,540))
+    
     while(vid.isOpened()):
         ret,frame = vid.read()
         if ret == False:
             break  
-    
         frame_post_mask = mask_poly(frame,poly_points)
         frame = find_lanes(frame,frame_post_mask,limits)
 
         count +=1
         cv.namedWindow('frame',cv.WINDOW_NORMAL)
         cv.imshow('frame',frame)
-        
-        # cv.waitKey(1)
-        cv.waitKey(0)
-        # break
+        out.write(frame)
+        cv.waitKey(1)
+        # cv.waitKey(0)
+
     vid.release()
 if __name__ == '__main__':
     main()               
